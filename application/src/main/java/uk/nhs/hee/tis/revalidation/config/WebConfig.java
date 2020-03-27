c88ead7c0fd39626024dc8d0a302d5a2a02fadc0
Uzair-HEE <uzair-hee@muhammads-mbp.home>
2020-03-27 15:18:46 +0000

    add cors for frontend calls

---
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/config/WebConfig.java
@@ -0,0 +1,26 @@
+package uk.nhs.hee.tis.revalidation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter addCorsMappings() {
+        final var source = new UrlBasedCorsConfigurationSource();
+
+        final var config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/swagger-ui.html", config);
+        return new CorsFilter(source);
+    }
+}

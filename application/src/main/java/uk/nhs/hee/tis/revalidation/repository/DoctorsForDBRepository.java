1df56ffeb6b812d06e25e69c076ee2329c64a544
Uzair <uzair.baig@hee.nhs.uk>
2020-04-08 11:50:17 +0100

    Use undernotice as enum

--- application/src/main/java/uk/nhs/hee/tis/revalidation/repository/DoctorsForDBRepository.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/repository/DoctorsForDBRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import uk.nhs.hee.tis.revalidation.entity.DoctorsForDB;
+import uk.nhs.hee.tis.revalidation.entity.UnderNotice;
 
 @Repository
 public interface DoctorsForDBRepository extends MongoRepository<DoctorsForDB, String> {
 
     //Get count for trainee doctors who are underNotice
-    long countByUnderNoticeIn(final String... underNotice);
+    long countByUnderNoticeIn(final UnderNotice... underNotice);
 
     //Get trainee doctors who are underNotice
-    Page<DoctorsForDB> findAllByUnderNoticeIn(final Pageable pageable, final String... underNotice);
+    Page<DoctorsForDB> findAllByUnderNoticeIn(final Pageable pageable, final UnderNotice... underNotice);
 }

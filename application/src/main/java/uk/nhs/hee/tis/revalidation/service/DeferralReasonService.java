f67a839fae637ee1f78dddceb17d5de847feeea3
Doris Wong <doris.wong@hee.nhs.uk>
2021-08-12 17:48:58 +0100

    feat: add historical Deferral Reasons
    TIS21-1952

--- application/src/main/java/uk/nhs/hee/tis/revalidation/service/DeferralReasonService.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/service/DeferralReasonService.java
@@ -29,10 +29,59 @@ public class DeferralReasonService {
   //insert DeferralReason's data in DB on startup
   public void insertDeferralReason() {
     final var insufficientEvidence = buildDeferralReason("1",
-        "Insufficient evidence for a positive recommendation", buildDeferralSubReason("1"));
+        "Insufficient evidence for a positive recommendation",
+        "INSUFFICIENT_EVIDENCE",
+        buildDeferralSubReason("1"),
+        Status.CURRENT);
     final var doctorSubjectToProcess = buildDeferralReason("2",
-        "The doctor is subject to an ongoing process", buildDeferralSubReason("2"));
-    deferralReasonRepository.saveAll(List.of(insufficientEvidence, doctorSubjectToProcess));
+        "The doctor is subject to an ongoing process",
+        "ONGOING_PROCESS",
+        buildDeferralSubReason("2"),
+        Status.CURRENT);
+    final var below1YearToCct = buildDeferralReason("3",
+        "Below 1 year to CCT",
+        "BELOW_1_YEAR_TO_CCT",
+        buildDeferralSubReason("3"),
+        Status.INACTIVE);
+    final var sickCarersLeave = buildDeferralReason("4",
+        "Sick carers leave",
+        "SICK_CARERS_LEAVE",
+        buildDeferralSubReason("4"),
+        Status.INACTIVE);
+    final var parentalLeave = buildDeferralReason("5",
+        "Parental leave",
+        "PARENTAL_LEAVE",
+        buildDeferralSubReason("5"),
+        Status.INACTIVE);
+    final var examfailure = buildDeferralReason("6",
+        "Exam failure",
+        "EXAM_FAILURE",
+        buildDeferralSubReason("6"),
+        Status.INACTIVE);
+    final var other = buildDeferralReason("7",
+        "Other",
+        "OTHER",
+        buildDeferralSubReason("7"),
+        Status.INACTIVE);
+    final var outOfClinicalTraining = buildDeferralReason("8",
+        "Out of Clinical training",
+        "OUT_OF_CLINICAL_TRAINING",
+        buildDeferralSubReason("8"),
+        Status.INACTIVE);
+    final var below5YearsFullReg = buildDeferralReason("9",
+        "Below 5 years full reg",
+        "BELOW_5_YEARS_FULL_REG",
+        buildDeferralSubReason("9"),
+        Status.INACTIVE);
+    deferralReasonRepository.saveAll(List.of(insufficientEvidence, 
+        doctorSubjectToProcess,
+        below1YearToCct,
+        sickCarersLeave,
+        parentalLeave,
+        examfailure,
+        other,
+        outOfClinicalTraining,
+        below5YearsFullReg));
   }
 
   //get All deferral reasons
@@ -63,12 +112,18 @@ public class DeferralReasonService {
     return deferralReason.getSubReasonByCode(reasonSubCode);
   }
 
-  private DeferralReason buildDeferralReason(final String code, final String reason,
-      final List<DeferralReason> subReason) {
+  private DeferralReason buildDeferralReason(final String code,
+      final String reason,
+      final String abbr,
+      final List<DeferralReason> subReason,
+      final Status status
+  ) {
     return DeferralReason.builder()
         .code(code)
         .reason(reason)
+        .abbr(abbr)
         .deferralSubReasons(subReason)
+        .status(status)
         .build();
   }
 

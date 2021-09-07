04184b7731de81aadeee8d16c49ee10a55b7e0e2
jayanta2018 <jayanta.saha@hee.nhs.uk>
2021-09-07 15:15:47 +0100

    fix: setting the correct actual submission date TIS21-2077

--- application/src/test/java/uk/nhs/hee/tis/revalidation/service/RecommendationServiceTest.java
+++ application/src/test/java/uk/nhs/hee/tis/revalidation/service/RecommendationServiceTest.java
@@ -1,3 +1,24 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2021 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uk.nhs.hee.tis.revalidation.service;
 
 import static java.util.Optional.of;
@@ -35,7 +56,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -717,6 +737,7 @@ class RecommendationServiceTest {
 
     assertThat(recommendationResult.getGmcNumber(), is(gmcId));
     assertThat(recommendationResult.getGmcSubmissionDate(), is(submissionDate));
+    assertThat(recommendationResult.getActualSubmissionDate(), is(actualSubmissionDate));
     assertThat(recommendationResult.getComments(), is(comments));
     assertThat(recommendationResult.getAdmin(), is(admin1));
   }
